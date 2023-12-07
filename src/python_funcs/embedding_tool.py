58f700823946c7b2fc54f6936b4398cba58ce9df
StirlingSmith <stirlingjamessmith@gmail.com>
2023-12-08 09:44:50 +1300

    julia python integration

--- src/python_funcs/embedding_tool.py
+++ src/python_funcs/embedding_tool.py
@@ -5,3 +5,8 @@ def embedding_tool(text):
     embeddings = OpenAIEmbeddings()
     query_result = embeddings.embed_query(text)
     return query_result
+
+def embedding_tol(text):
+    embeddings = OpenAIEmbeddings()
+    query_result = embeddings.embed_query(text)
+    return query_result
