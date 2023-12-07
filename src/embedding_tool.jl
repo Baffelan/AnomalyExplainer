9c06fb813a93efa5e062b49b3e3eb4db466ac035
StirlingSmith <stirlingjamessmith@gmail.com>
2023-12-08 12:42:49 +1300

    remove unused code

--- src/embedding_tool.jl
+++ src/embedding_tool.jl
@@ -5,12 +5,7 @@ Returns the LLM embedding using the embedding_tool.pyc
 - `text::String`
 """
 function embedding_tool(text::String)
-    val = @pyexec """
-        import sys
-        sys.path.insert(1, './src/python_funcs')
-        import embedding_tool
-        embedding_tool.embedding_tool"""
     emb_tool = pyimport("embedding_tool")
 
-    return pyconvert(Vector,embt.embedding_tool(pystr(text)))
+    return pyconvert(Vector,emb_tool.embedding_tool(pystr(text)))
 end
--- src/explain.jl
+++ src/explain.jl
@@ -4,14 +4,9 @@ Runs the llm query.
 # Arguments
 - `query::String`: The query string for the llm to answer.
 """
-function explain(query)
-    val = @pyexec """
-        import sys
-        sys.path.insert(1, './src/python_funcs')
-        import explainer_tool
-        explainer_tool.explainer_tool"""
+function explain(query, article)
     exp_tool = pyimport("explainer_tool")
+    println(length(article))
 
-    return pyconvert(Vector,exp_tool.explainer_tool(pystr(query)))
+    return (pyconvert(String,exp_tool.explain(pystr(query), pystr(article), pystr("gpt-4-1106-preview"))), article)
 end
-
