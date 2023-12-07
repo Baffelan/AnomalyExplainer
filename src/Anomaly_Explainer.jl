0d9c117e05b7716f275831ca321b751356fd3aad
StirlingSmith <stirlingjamessmith@gmail.com>
2023-12-08 12:41:58 +1300

    include and export

--- src/Anomaly_Explainer.jl
+++ src/Anomaly_Explainer.jl
@@ -6,7 +6,19 @@ module Anomaly_Explainer
     using Dates
 
     const pyglobals = PyDict()
-    #include("Contextualiser/embedding_tool.jl")
+    include("Contextualiser/get_context.jl")
+    include("Contextualiser/search_db.jl")
+
+    include("ingest_data/chunkise_text.jl")
+    include("ingest_data/ingest_news.jl")
+    include("ingest_data/save_to_db.jl")
+
+    include("embedding_tool.jl")
+    include("explain.jl")
+    include("explain_anomaly.jl")
+
+    export explain_anomaly
+    
 end
 
 
