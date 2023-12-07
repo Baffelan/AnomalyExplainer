0b74ec94c4da0ba40db8e9b952f30a1b5926e369
StirlingSmith <stirlingjamessmith@gmail.com>
2023-12-08 12:44:34 +1300

    added comment for pontentially useful code

--- src/ingest_data/save_to_db.jl
+++ src/ingest_data/save_to_db.jl
@@ -7,3 +7,26 @@ Saves a `dbrow`` to the `vectorembeddingdb`.
 function save_to_db(dbrow::AbstractVector)
 
 end
+
+# # Connect to postgres (vector database not set up yet)
+# using Octo
+# using LibPQ
+
+# conn=LibPQ.Connection("dbname=vectorembeddingdb host=Localhost user=postgres password=$(ENV["LOCALPASSWORD"])")
+
+# execute(conn, "create table posts (
+#     id serial primary key,
+#     title text not null,
+#     body text not null,
+#     embedding vector(1536)
+#   );")
+
+# execute(conn, "drop table posts;")
+
+# execute(conn, "INSERT INTO posts (id, title, body, embedding) values (default, 'testing', '$(replace(news2,"'"=>""))', '$(emb2)');")
+
+
+
+# news3 = JSON.parsefile("Data/amazon_new_data.json")[55]["body"]
+
+# DataFrame(test)
