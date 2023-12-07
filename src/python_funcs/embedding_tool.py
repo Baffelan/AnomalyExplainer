8329000f1a4250aa15625c44fb05d85851883c2d
StirlingSmith <stirlingjamessmith@gmail.com>
2023-12-08 12:45:02 +1300

    fixed python import issue

--- src/python_funcs/embedding_tool.py
+++ src/python_funcs/embedding_tool.py
@@ -5,8 +5,3 @@ def embedding_tool(text):
     embeddings = OpenAIEmbeddings()
     query_result = embeddings.embed_query(text)
     return query_result
-
-def embedding_tol(text):
-    embeddings = OpenAIEmbeddings()
-    query_result = embeddings.embed_query(text)
-    return query_result
