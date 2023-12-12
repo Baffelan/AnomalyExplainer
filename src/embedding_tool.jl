9863d69429a872a92d9463b83ee42c0c5332a736
StirlingSmith <stirlingjamessmith@gmail.com>
2023-12-13 12:42:38 +1300

    Adding modules to python path

--- src/embedding_tool.jl
+++ src/embedding_tool.jl
@@ -5,6 +5,10 @@ Returns the LLM embedding using the embedding_tool.pyc
 - `text::String`
 """
 function embedding_tool(text::String)
+    @pyexec """import sys
+    # caution: path[0] is reserved for script path (or '' in REPL)
+    sys.path.insert(1, './src')
+    """
     emb_tool = pyimport("embedding_tool")
 
     return pyconvert(Vector,emb_tool.embedding_tool(pystr(text)))
